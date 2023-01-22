08944dca79031f2202ef8457cacfb4230ab56f54
Fkmoya2794 <123271072+Fkmoya2794@users.noreply.github.com>
2023-01-22 19:00:20 -0300

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,15 @@
+
+//Muestra lista de aptitudes
+document.getElementById("boton1").addEventListener("click", function() {
+    document.getElementById("aptitudes").style.display = "block";
+});
+
+//Muestra lista de actitudes
+document.getElementById("boton2").addEventListener("click", function() {
+    document.getElementById("actitudes").style.display = "block";
+});
+
+//Aumenta tamaño letra
+document.getElementById('boton').addEventListener("click", function() {
+    document.getElementById("cuerpo").style.fontSize='25px';
+})
